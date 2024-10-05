be9746aa1800b8ba1a3a1aa5004abf8ea9545f1f
段元哲 <1342558165@qq.com>
2024-10-06 01:08:31 +0800

    feat:添加各模块

---
+++ yoj-backend-gateway/src/main/java/com/yupi/yojbackendgateway/YojBackendGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.yojbackendgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YojBackendGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YojBackendGatewayApplication.class, args);
+    }
+
+}
